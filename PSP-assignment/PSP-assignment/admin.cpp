3a6d7eee35e1c21061eb7d2e5184f63f2eb51af5
lohxt <lohxt-wp24@student.tarc.edu.my>
2025-07-31 21:31:43 +0800

    t

---
+++ PSP-assignment/PSP-assignment/admin.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void Indi_schedule() {
+	int ex;
+	cout << "Select an expert"<<endl;
+	cout << "ex1\nex2\nex3"<<endl;
+	cin >> ex;
+
+	switch (ex)
+	{
+	case 1:
+		cout << "schedule1";
+		break;
+	case 2:
+		cout << "schedule2";
+		break;
+	case 3:
+		cout << "schedule3";
+		break;
+	}
+}
+void Schedule() {
+	cout << "full schedule here";
+}
+void Customer_ls() {
+	cout << "full customer list";
+}
+
+void Generate_sales_rpt() {
+
+}
+
+void Expert_bonus() {
+
+}
+
+int main() {
+	int choice;
+	cout << "What do you want to look at? "<<endl;
+	cout << "1. Individual expert schedule\n2. Overall schedule\n3. Customer list\n4. Generate sales reports\n5. Expert bonus entitlements\n";
+	cin >> choice;
+
+	switch (choice)
+		{
+		case 1:
+			Indi_schedule();
+			break;
+
+		case 2:
+			Schedule();
+			break;
+
+		case 3:
+			Customer_ls();
+			break;
+
+		case 4:
+			Generate_sales_rpt();
+			break;
+		
+		case 5:
+			Expert_bonus();
+			break;
+		
+			default:cout<<"nah";
+			break;
+		}
+	return 0;
+}
